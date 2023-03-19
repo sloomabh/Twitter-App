693651b4c65f178f74a6af7109df089cce4f7b64
pej.benhalima <pej.benhalima@gmail.com>
2023-03-19 21:14:11 +0900

    modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
+import LoginMoadal from "@/components/modals/LoginMoadal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <LoginModal />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
