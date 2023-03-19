8f9495f2830604332d12da802812299135742c98
salimbh <sloomabenhalima95@gmail.com>
2023-03-19 12:18:51 +0900

    setup 2

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,13 +4,13 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 
-const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="h-screen bg-black ">
       <div className="container  h-full mx-auto xl:px-30 max-w-6xl">
-        <div className="grid grid-colos-4 h-full">
+        <div className="grid grid-cols-4 h-full">
           <Sidebar />
-          <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
+          <div className="col-span-3 lg:col-span-2 border-x-[3px]  border-neutral-800">
             {children}
           </div>
         </div>
--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+//import { signOut } from "next-auth/react";
+import { BiLogOut } from "react-icons/bi";
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
 
-function Sidebar() {
-  return <div>Sidebar</div>;
-}
+const Sidebar = () => {
+  const items = [
+    {
+      icon: BsHouseFill,
+      label: "Home",
+      href: "/",
+    },
+    {
+      icon: BsBellFill,
+      label: "Notifications",
+      href: "/notifications",
+    },
+    {
+      icon: FaUser,
+      label: "Profile",
+      href: "/users/123",
+      auth: true,
+    },
+  ];
+  return <div></div>;
+};
 
 export default Sidebar;
