4c02f540810adc4d7227130a52ee373340465399
pej.benhalima <pej.benhalima@gmail.com>
2023-03-24 18:00:38 +0900

    edit modal

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -16,9 +16,9 @@ interface UserBioProps {
 const UserBio: React.FC<UserBioProps> = ({ userId }) => {
   const { data: currentUser } = useCurrentUser();
   const { data: fetchedUser } = useUser(userId);
-  /*
-  const editModal = useEditModal();
 
+  const editModal = useEditModal();
+  /*
   const { isFollowing, toggleFollow } = useFollow(userId);
 */
   const createdAt = useMemo(() => {
@@ -33,11 +33,7 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
     <div className="border-b-[1px] border-neutral-800 pb-4">
       <div className="flex justify-end p-2">
         {currentUser?.id === userId ? (
-          <Button
-            secondary
-            label="Edit"
-            onClick={() => {} /*editModal.onOpen*/}
-          />
+          <Button secondary label="Edit" onClick={editModal.onOpen} />
         ) : (
           <Button
             onClick={() => {} /* toggleFollow */}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,16 +6,15 @@ import Layout from "@/components/Layout";
 import LoginModal from "@/components/modals/LoginModal";
 import RegisterModal from "@/components/modals/RegisterModal";
 import "@/styles/globals.css";
-//import EditModal from '@/components/modals/EditModal';
+import EditModal from "@/components/modals/EditModal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Toaster />
+      <EditModal />
       <RegisterModal />
       <LoginModal />
-
-      {/* <EditModal />  */}
       <Layout>
         <Component {...pageProps} />
       </Layout>
